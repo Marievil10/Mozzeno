da7f43855798e5b3ef186af6a5154c56f02343f7
Marie Perin <marieperin0611@gmail.com>
2024-12-01 15:37:24 +0100

    - included another fixed value

--- fixed_values.py
+++ fixed_values.py
@@ -9,8 +9,8 @@ start_capital = 500
 language_moz = 'NL'
 
 # these values are from the Mozzeno site
-est_bruto = [4.75, 5.05, 5.15, 5.25, 5.35, 5.45, 5.55, 5.65, 5.75, 5.85, 5.95, 6.05]
-est_netto = [3.30, 3.51, 3.58, 3.65, 3.72, 3.79, 3.85, 3.92, 4.00, 4.06, 4.13, 4.20]
+est_bruto = [4.75, 5.05, 5.15, 5.25, 5.35, 5.45, 5.55, 5.65, 5.75, 5.85, 5.95, 6.05, 6.10]
+est_netto = [3.30, 3.51, 3.58, 3.65, 3.72, 3.79, 3.85, 3.92, 4.00, 4.06, 4.13, 4.20, 4.23]
 
 df_percent = pd.DataFrame(list(zip(est_bruto, est_netto)),
                           columns=['Bruto', 'Netto'])
